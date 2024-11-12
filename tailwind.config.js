90adb5fe60274551e0e6584531a64c57880ecfd7
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-12 11:09:33 +0530

    Set up fonts and colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./*.{html, js}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins", sans-serif'],
+      },
+      colors: {
+        frontend: {
+          purple: "hsl(259, 100 %, 65 %)",
+          "light-red": "hsl(0, 100%, 67%)",
+          "off-white": "hsl(0, 100%, 67%)",
+          "light-grey": "hsl(0, 0%, 86%)",
+          "smokey-grey": "hsl(0, 1%, 44%)",
+          "off-black": "hsl(0, 0%, 8%)",
+        },
+      },
+    },
+  },
+  plugins: [],
 };
