9c670844a13d2719372863c7c86f89ff7156e2a5
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-13 14:50:17 +0530

    Attempt to figure out difference of years and months unsuccessfully

--- script.js
+++ script.js
@@ -4,12 +4,48 @@ const dayInput = document.querySelector("#day-input");
 const monthInput = document.querySelector("#month-input");
 const yearInput = document.querySelector("#year-input");
 const submitButton = document.querySelector("button");
+let day = 0;
+let month = 0;
+let year = 0;
+let dayDifference = 0;
+let monthDifference = 0;
+let yearDifference = 0;
 
 submitButton.addEventListener("click", () => {
-  inputDate.setFullYear(
-    parseInt(yearInput.value),
-    parseInt(monthInput.value) - 1,
-    parseInt(dayInput.value),
+  year = parseInt(yearInput.value);
+  month = parseInt(monthInput.value);
+  day = parseInt(dayInput.value);
+
+  monthDifference = getMonthDifference(
+    currentDate.getMonth(),
+    month,
+    currentDate.getDate(),
+    day,
   );
-  console.log(inputDate);
+
+  yearDifference = getYearDifference(
+    currentDate.getFullYear(),
+    year,
+    currentDate.getMonth(),
+    month,
+  );
+
+  console.log(monthDifference);
+  console.log(`Years: ${yearDifference}`);
 });
+
+function getMonthDifference(currentMonth, inputMonth, currentDay, inputDay) {
+  if (currentDay < inputDay) {
+    return currentMonth - inputMonth;
+  } else {
+    return currentMonth - inputMonth + 1;
+  }
+}
+
+function getYearDifference(currentYear, inputYear, currentMonth, inputMonth) {
+  if (currentMonth < inputMonth) {
+    return currentYear - inputYear - 1;
+  } else {
+    return currentYear - inputYear;
+  }
+}
