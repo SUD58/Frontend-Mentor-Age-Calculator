30ec2e2baad61ee0cbceb979394fd2af21a7a711
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-15 16:59:23 +0530

    Attempt again to figure out difference of years and months unsuccessfully

--- script.js
+++ script.js
@@ -1,51 +1,37 @@
 let inputDate = new Date();
+let day = 0;
+let month = 0;
+let year = 0;
+let difference = 0;
+let yearDifference = 0;
+let monthDifference = 0;
+let dayDifference = 0;
+
 const currentDate = new Date();
 const dayInput = document.querySelector("#day-input");
 const monthInput = document.querySelector("#month-input");
 const yearInput = document.querySelector("#year-input");
 const submitButton = document.querySelector("button");
-let day = 0;
-let month = 0;
-let year = 0;
-let dayDifference = 0;
-let monthDifference = 0;
-let yearDifference = 0;
 
 submitButton.addEventListener("click", () => {
   year = parseInt(yearInput.value);
   month = parseInt(monthInput.value);
   day = parseInt(dayInput.value);
+  inputDate.setFullYear(year, month, day);
+  console.log(currentDate.getTime());
 
-  monthDifference = getMonthDifference(
-    currentDate.getMonth(),
-    month,
-    currentDate.getDate(),
-    day,
-  );
+  difference = currentDate.getTime() - inputDate.getTime();
+  yearDifference = Math.floor(difference / (1000 * 60 * 60 * 24 * 365.25));
+  difference -= yearDifference * (1000 * 60 * 60 * 24 * 365.25);
+  console.log(difference);
 
-  yearDifference = getYearDifference(
-    currentDate.getFullYear(),
-    year,
-    currentDate.getMonth(),
-    month,
-  );
+  monthDifference = Math.floor(difference / (1000 * 60 * 60 * 24 * 30.44));
+  difference -= monthDifference * (1000 * 60 * 60 * 24 * 30.44);
+  console.log(difference);
 
-  console.log(monthDifference);
-  console.log(`Years: ${yearDifference}`);
+  dayDifference = Math.floor(difference / (1000 * 60 * 60 * 24));
+  difference -= dayDifference * (1000 * 60 * 60 * 24);
+  console.log(difference);
+
+  console.log({ yearDifference, monthDifference, dayDifference });
 });
-
-function getMonthDifference(currentMonth, inputMonth, currentDay, inputDay) {
-  if (currentDay < inputDay) {
-    return currentMonth - inputMonth;
-  } else {
-    return currentMonth - inputMonth + 1;
-  }
-}
-
-function getYearDifference(currentYear, inputYear, currentMonth, inputMonth) {
-  if (currentMonth < inputMonth) {
-    return currentYear - inputYear - 1;
-  } else {
-    return currentYear - inputYear;
-  }
-}
